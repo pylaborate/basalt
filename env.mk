6cbb2bb03e35cb606f93353da3e41bc9bcfbe6e6
Sean Champ <spchamp@users.noreply.github.com>
2023-05-23 00:37:09 -0700

    Makefile debugging for GitHub workflow actions on windows

--- env.mk
+++ env.mk
@@ -141,7 +141,8 @@ ${pip-tools-sync_stamp}: ${REQ_TXT} ${ENV_CMD_pip_SYNC}
 ${ENV_CFG}: ${INSTALL_ENV}
 	if ! [ -e ${ENV_CFG} ]; then \
 		${HOST_PYTHON} ${INSTALL_ENV} ${ENV_DIR}; \
-		${ENV_pip} install pipenv; \
+		if ! ${ENV_pip} install pipenv; then 
+			echo "virtualenv installation failed on ${RUNNER_OS}"; false; fi; \
 	fi
 
 ## generate a requirements.txt as a composite of project/user requirements
