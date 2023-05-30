771a965edbacacef5a98e5c23ecdc4707b9dc546
Sean Champ <spchamp@users.noreply.github.com>
2023-05-29 22:47:09 -0700

    pylaborate namespace package: Adding docstring

--- src/pylaborate/__init__.py
+++ src/pylaborate/__init__.py
@@ -1,5 +1,12 @@
+'''`pylaborate` namespace package, via the `pyalborate.basalt` distribution
 
-## pylaborate namespace definition @ basalt
+## Overview
+
+The `pylaborate` package represents a namespace package.
+
+This definition of the namespace package is provided via the `pylaborate.basalt`
+distribution.
+'''
 
 from pkg_resources import declare_namespace
 declare_namespace(__name__)
